369af733d2a76d3214aae3a6a25276d38aed9334
Zabir Raihan <zabirraihan@icloud.com>
2026-05-10 15:12:03 +0600

    listrowseparator conditions in verse list

--- Iqra/Views/SurahList/SurahDetailView.swift
+++ Iqra/Views/SurahList/SurahDetailView.swift
@@ -31,6 +31,8 @@ struct SurahDetailView: View {
                 ForEach(verses, id: \.id) { verse in
                     VerseRow(verse: verse, surahTitle: surah.translation)
                         .id(scrollIdentifier(for: verse))
+                        .listRowSeparator(.hidden, edges: .top)
+                        .listRowSeparator(.visible, edges: .bottom)
                     #if os(macOS)
                         .padding(7)
                     #endif
